a097dc15854569752688a8096239b4f8a141dc70
Max Podkorytov <max.podkorytoff@yandex.ru>
2014-09-21 23:54:52 +0400

    project directory cleanup

--- function.cpp
+++
@@ -1,9 +0,0 @@
-#include <qmath.h>
-
-qreal function(qreal x, qreal y) {
-	return qExp (x*x - y*y);
-	return (x + y);
-	return 1;
-	return (sin(3 * x) - sin(2 * y)) / 2;
-	return qExp (x + y) - 5;
-}
--- function.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef FUN_HPP
-#define FUN_HPP
-
-double function(double x, double y);
-
-#endif
